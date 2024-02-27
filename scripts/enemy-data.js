ccaf1927cb1033561dca675d96bea4a8f3ecf760
Jade <asleepydoggy@gmail.com>
2024-02-28 00:30:36 +1100

    style: format enemy data file

--- scripts/enemy-data.js
+++ scripts/enemy-data.js
@@ -1,57 +1,57 @@
 const enemyData = {
-    "SWAT": {
-        "Health": 150,
-        "Armor": 70,
-        "ArmorHardness": 1.5
-    },
-    "Heavy SWAT": {
-        "Health": 150,
-        "Armor": 170,
-        "ArmorHardness": 1.5
-    },
-    // Specials:
-    // "Zapper": {
-    //     "Health": 150,
-    //     "Armor": 140,
-    //     "ArmorHardness": 1.5
-    // },
-    // "Grenadier": {
-    //     "Health": 150,
-    //     "Armor": 140,
-    //     "ArmorHardness": 1.5
-    // },
-    "Specials": {
-        "Health": 150,
-        "Armor": 140,
-        "ArmorHardness": 1.5
-    },
-    "Sniper": {
-        "Health": 40,
-        "Armor": 0,
-        "ArmorHardness": 0
-    },
-    "Cloaker": {
-        "Health": 150,
-        "Armor": 0,
-        "ArmorHardness": 0
-    },
-    "Drone": {
-      "Health": 50,
-      "Armor": 150,
-      "ArmorHardness": 2
-    },
-    "Shield": {
-        "Health": 160,
-        "Armor": 180,
-        "VisorArmor": 250,
-        "ArmorHardness": 2,
-        "VisorArmorHardness": 1.75
-    },
-    "Bulldozer": {
-        "Health": 200,
-        "Armor": 3500,
-        "VisorArmor": 850,
-        "ArmorHardness": 4,
-	    "VisorArmorHardness": 4
-    }
-}
+  SWAT: {
+    Health: 150,
+    Armor: 70,
+    ArmorHardness: 1.5,
+  },
+  "Heavy SWAT": {
+    Health: 150,
+    Armor: 170,
+    ArmorHardness: 1.5,
+  },
+  // Specials:
+  // "Zapper": {
+  //   "Health": 150,
+  //   "Armor": 140,
+  //   "ArmorHardness": 1.5
+  // },
+  // "Grenadier": {
+  //   "Health": 150,
+  //   "Armor": 140,
+  //   "ArmorHardness": 1.5
+  // },
+  Specials: {
+    Health: 150,
+    Armor: 140,
+    ArmorHardness: 1.5,
+  },
+  Sniper: {
+    Health: 40,
+    Armor: 0,
+    ArmorHardness: 0,
+  },
+  Cloaker: {
+    Health: 150,
+    Armor: 0,
+    ArmorHardness: 0,
+  },
+  Drone: {
+    Health: 50,
+    Armor: 150,
+    ArmorHardness: 2,
+  },
+  Shield: {
+    Health: 160,
+    Armor: 180,
+    VisorArmor: 250,
+    ArmorHardness: 2,
+    VisorArmorHardness: 1.75,
+  },
+  Bulldozer: {
+    Health: 200,
+    Armor: 3500,
+    VisorArmor: 850,
+    ArmorHardness: 4,
+    VisorArmorHardness: 4,
+  },
+};
