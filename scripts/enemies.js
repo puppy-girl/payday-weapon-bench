bca653735c9c5d15c3e93e1c0d7d799061172a1d
Jade <asleepydoggy@gmail.com>
2024-07-22 19:14:08 +1000

    chore: update enemy data

---
+++ scripts/enemies.js
@@ -0,0 +1,46 @@
+const enemyData = {
+    "SWAT": {
+        "Health": 150,
+        "Armor": 70,
+        "ArmorHardness": 1.5
+    },
+    "Heavy SWAT": {
+        "Health": 150,
+        "Armor": 70,
+        "ArmorHardness": 1.5
+    },
+    "Specials": {
+        "Health": 150,
+        "Armor": 140,
+        "ArmorHardness": 1.5
+    },
+    "Shield": {
+        "Health": 160,
+        "Armor": 180,
+        "ArmorHardness": 2,
+        "VisorArmor": 250,
+        "VisorArmorHardness": 1.75 
+    },
+    "Bulldozer": {
+        "Health": 200,
+        "Armor": 3500,
+        "ArmorHardness": 4,
+        "VisorArmor": 850,
+        "VisorArmorHardness": 4
+    },
+    "Sniper": {
+        "Health": 40,
+        "Armor": 0,
+        "ArmorHardness": 0
+    },
+    "Cloaker": {
+        "Health": 150,
+        "Armor": 0,
+        "ArmorHardness": 0
+    },
+    "Drone": {
+        "Health": 50,
+        "Armor": 150,
+        "ArmorHardness": 2
+    }
+}
